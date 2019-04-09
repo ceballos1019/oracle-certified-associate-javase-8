9af151d06cfddf4673c98c16f7c04363a5ca2a0e
Andres Ceballos Sanchez <andres.ceballos@ibm.com>
2019-04-09 18:29:32 -0500

    Se agrega un ejemplo con la estructura básica de una clase

---
+++ objects-classes/MyClass.java
@@ -0,0 +1,29 @@
+package objects-classes;
+
+/**
+ * Shows the structure of a class
+ * @author Andres Ceballos Sanchez - andresrxn10@hotmail.com
+ * @version 1.0
+ * @since 2019/04/09
+ */
+//Class declaration must use the keyword class followed by a class name
+public class MyClass {
+
+    /* Fields: represents the object state*/
+    String color;
+    int age;
+
+    /* Methods: represents the object behavior*/
+    public void methodA(){};
+    public void methodB(){};
+    
+    /**
+     * Entry point to the application
+     */
+    public static void main(String[] args) {
+
+        /* Simply declaring a reference variable does not create an object. */
+        MyClass declaredVariable;
+        
+    }
+}
